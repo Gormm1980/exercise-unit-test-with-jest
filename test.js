0256a4adae84250ce4e0962896e80ea44df04536
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-05-26 17:32:10 +0000

    descomentados los test

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
-// test("One euro should be 1.206 dollars", function(){
-//     //import the function from app.js
-//     const { fromEuroToDollar } = require('./app.js')
+test("One euro should be 1.206 dollars", function(){
+    //import the function from app.js
+    const { fromEuroToDollar } = require('./app.js')
 
-//     // use the function like its suppoed to be used
-//     const dollars = fromEuroToDollar(3.5)
+    // use the function like its suppoed to be used
+    const dollars = fromEuroToDollar(3.5)
 
-//     // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
-//     const expected = 3.5 * 1.2; 
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    const expected = 3.5 * 1.2; 
     
-//     // this is the comparison for the unit test
-//     expect(expected).toBe(dollars);})
+    // this is the comparison for the unit test
+    expect(expected).toBe(dollars);})
     
 
-    // test("One Dollar should be 109.36 Yens", function(){
-    // //import the function from app.js
-    // const { fromDollarToYen } = require('./app.js')
+    test("One Dollar should be 109.36 Yens", function(){
+    //import the function from app.js
+    const { fromDollarToYen } = require('./app.js')
 
-    // // use the function like its suppoed to be used
-    // const Yens = fromDollarToYen(3.5)
+    // use the function like its suppoed to be used
+    const Yens = fromDollarToYen(3.5)
 
-    // // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
-    // const expected = 3.5 * 109.36; 
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    const expected = 3.5 * 109.36; 
     
-    // // this is the comparison for the unit test
-    // expect(expected).toBe(Yens);})
+    // this is the comparison for the unit test
+    expect(expected).toBe(Yens);})
 
     test("One Yen should be 0.0065 Yens", function(){
     //import the function from app.js
